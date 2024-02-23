dadfb109f74b9a356323c7d49099e92261e8ca7e
sebas0729 <730255@student.dusd.net>
2024-02-23 13:49:16 -0800

    Added 2nd c# script
    added script for car movement

--- unit1playercontrol/Assets/scripts/player controller.cs
+++ unit1playercontrol/Assets/scripts/player controller.cs
@@ -11,9 +11,16 @@ public class playercontroller : MonoBehaviour
     }
 
     // Update is called once per frame
+    public float speed = 20;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
     }
 }
